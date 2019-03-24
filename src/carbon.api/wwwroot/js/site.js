55b5442193d746ffee3551353f2abf2247f31b45
Owen Holloway <owen.holloway101@gmail.com>
2019-03-24 16:54:43 +1100

    API

---
+++ src/carbon.api/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems,{
+        format: 'dd mmm yyyy',
+        minDate: new Date(1930, 0 ,1),
+        maxDate: new Date(2008, 0 ,1),
+        yearRange: 60
+    });
+});
