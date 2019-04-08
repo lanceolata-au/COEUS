997ed8223ed3acd5b06005d1151a9e4820d046f4
Owen Holloway <owen.holloway101@gmail.com>
2019-04-08 15:29:00 +1000

    removed unused

--- src/carbon.persistence/transforms/scripts/testdata/20190323_test.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '100');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '101');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '102');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '103');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '104');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '105');
-INSERT INTO `test` (`name`, `test`) VALUES ('DEFAULT', '106');
