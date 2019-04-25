4dc5eb602baa95e412e5d41235553b13b49f291c
Owen Holloway <owen.holloway101@gmail.com>
2019-04-25 19:13:57 +1000

    Transforms

--- src/carbon.persistence/transforms/scripts/transforms/20190323_test.sql
+++ src/carbon.persistence/transforms/scripts/transforms/20190323_test.sql
@@ -1,4 +1,18 @@
 CREATE TABLE test(
+  id char(36) NULL,
   name varchar(128) NOT NULL DEFAULT 'DEFAULT',
   value int NOT NULL DEFAULT 100
-)
+);
+
+DELIMITER ;;
+CREATE TRIGGER test_id
+  BEFORE INSERT ON test
+  FOR EACH ROW
+BEGIN
+  IF new.id IS NULL THEN
+    SET new.id = uuid();
+  END IF;
+END
+;;
+
+DELIMITER ;
--- src/carbon.persistence/transforms/scripts/transforms/20190409_test.sql
+++ src/carbon.persistence/transforms/scripts/transforms/20190409_test.sql
@@ -1,4 +1,19 @@
 CREATE TABLE test_2(
+  id char(36) NULl,
   name varchar(128) NOT NULL DEFAULT 'DEFAULT',
   value int NOT NULL DEFAULT 100
-)
+);
+
+
+DELIMITER ;;
+CREATE TRIGGER test_2_id
+  BEFORE INSERT ON test_2
+  FOR EACH ROW
+BEGIN
+  IF new.id IS NULL THEN
+    SET new.id = uuid();
+  END IF;
+END
+;;
+
+DELIMITER ;
