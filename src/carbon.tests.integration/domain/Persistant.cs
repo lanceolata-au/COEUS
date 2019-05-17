a6b5d504aabc91d3ec723ce34ee36ba5c682b330
Owen Holloway <owen.holloway101@gmail.com>
2019-05-17 21:28:06 +1000

    Tests

---
+++ src/carbon.tests.integration/domain/Persistant.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace carbon.tests.integration.domain
+{
+    public class Persistant
+    {
+        [Test, Ignore("Persistence Down")]
+        public void IsPersisting()
+        {
+            
+        }
+    }
+}
