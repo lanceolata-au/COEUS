311668204392f7952fc4ec26421e32c473ec2a03
Owen Holloway <owen.holloway101@gmail.com>
2019-05-17 23:35:33 +1000

    Setup for DB

--- src/carbon.tests.integration/domain/Persistent.cs
+++ src/carbon.tests.integration/domain/Persistent.cs
@@ -1,12 +1,28 @@
+using carbon.persistence.transforms;
+using carbon.core.domain.model;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Test = carbon.core.domain.model.Test;
 
 namespace carbon.tests.integration.domain
 {
+    [TestFixture]
     public class Persistent
     {
-        [Test, Ignore("Persistence Down")]
+
+        [SetUp]
+        public void TestDatabaseUp()
+        {
+            var obj = new Runner(@"server=zeryter.xyz;user=carbonTest;password=the_game", false, false);
+        }
+        
+        [Test]
         public void IsPersisting()
         {
+            var obj = Test.Create();
+            
+            
+            Assert.Fail(obj.Name);
             
         }
     }
