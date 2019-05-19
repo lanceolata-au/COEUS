df287cfe3f24d41f6d8c01e490915016e1e46159
Owen Holloway <owen.holloway101@gmail.com>
2019-05-19 21:57:12 +1000

    Identity Server 4 DB tables

---
+++ src/carbon.api/Services/IdentitySetup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using carbon.api.Features;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace carbon.api.Services
+{
+    public class IdentitySetup
+    {
+        public static void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var  identityContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationIdentityDbContext>();
+                identityContext.Database.Migrate();
+
+                if (!identityContext.Users.Any())
+                {
+                    var adminUser = new IdentityUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = "owen.holloway101+infinity@gmail.com",
+                        UserName = "admin@zeryter.xyz",
+                        NormalizedUserName = "admin@zeryter.xyz",
+                        EmailConfirmed = true
+                    };
+
+                    adminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(adminUser, "Password1@");
+                    
+                    identityContext.Users.Add(adminUser);
+                    identityContext.SaveChanges(); 
+                }
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    
+                    foreach (var client in IdentityConfig.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityConfig.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in IdentityConfig.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- src/carbon.api/Startup.cs
+++ src/carbon.api/Startup.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
+using Autofac;
+using carbon.api.Features;
+using carbon.api.Services;
+using carbon.persistence.modules;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -29,14 +36,53 @@ namespace carbon.api
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            
+            /*
+             * Identity Server 4
+             * http://docs.identityserver.io
+             *
+             * Used for client credentials and authorisation access
+             *
+             * OpenID/oAuth2 based for ASP.net
+             */
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //START =-=-= DO NOT MODIFY UNLESS DISCUSSED USER AUTH IS HERE =-=-= START
+            
+            Console.WriteLine("ConfigureServices Start");
+            
+            //Startup Autofac
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            
+            services.AddDbContext<ApplicationIdentityDbContext>(options 
+                => options.UseMySql(Configuration.GetConnectionString("ApplicationDatabase"),
+                    optionsBuilder => optionsBuilder.MigrationsAssembly(migrationsAssembly)));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                .AddDefaultTokenProviders();
+            
+            services.AddIdentityServer()
+                .AddOperationalStore(options => options
+                    .ConfigureDbContext = optionsBuilder => optionsBuilder
+                    .UseMySql(Configuration.GetConnectionString("ApplicationDatabase"),sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)))
+                .AddConfigurationStore(options => options
+                    .ConfigureDbContext = optionsBuilder => optionsBuilder
+                    .UseMySql(Configuration.GetConnectionString("ApplicationDatabase"),sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)))
+                .AddDeveloperSigningCredential();
+            
+            services.AddMvc();
+
+            services.AddAuthorization();
+            
+            Console.WriteLine("ConfigureServices Completed");
+
+            //  END =-=-= DO NOT MODIFY UNLESS DISCUSSED USER AUTH IS HERE =-=-= END
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            Console.WriteLine("Startup Configured Start");
+            Console.WriteLine("Configure Start");
             
             if (env.IsDevelopment())
             {
@@ -51,6 +97,8 @@ namespace carbon.api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            IdentitySetup.InitializeDatabase(app);
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -63,8 +111,18 @@ namespace carbon.api
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
             
-            Console.WriteLine("Startup Configured End");
+            Console.WriteLine("Configure End");
             
+        }
+        
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            Console.WriteLine("ConfigureContainer Start");
+
+            builder.RegisterModule(new Persistence());
+            builder.RegisterAssemblyModules(AppScanner.GetCarbonAssemblies());
+            
+            Console.WriteLine("ConfigureContainer End");
         }
     }
 }
