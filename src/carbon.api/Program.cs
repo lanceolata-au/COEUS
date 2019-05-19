45fe034836f499e6acf92881e7af89f648402f97
Owen Holloway <owen.holloway101@gmail.com>
2019-05-19 15:41:40 +1000

    Trying to get an error page to work

--- src/carbon.api/Program.cs
+++ src/carbon.api/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace carbon.api
 {
@@ -14,11 +8,14 @@ namespace carbon.api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureServices(s => s.AddAutofac())
+                .UseStartup<Startup>()
+                .Build();
     }
 }
