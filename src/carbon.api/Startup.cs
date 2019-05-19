103e97035faa6ccac3783c0b82b0b658b7de886f
Owen Holloway <owen.holloway101@gmail.com>
2019-05-19 17:00:11 +1000

    More error page stuff

--- src/carbon.api/Startup.cs
+++ src/carbon.api/Startup.cs
@@ -41,7 +41,7 @@ namespace carbon.api
             if (env.IsDevelopment())
             {
                 //app.UseDeveloperExceptionPage();
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
