03be8b75e92a5051e8c56b18730ef1216ee4d6c3
Owen Holloway <owen.holloway101@gmail.com>
2019-07-19 17:10:55 +1000

    Added core components

---
+++ src/carbon.app/ClientApp/src/app/nav-footer/nav-footer.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-footer',
+  templateUrl: './nav-footer.component.html',
+  styleUrls: ['./nav-footer.component.css']
+})
+export class NavFooterComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
