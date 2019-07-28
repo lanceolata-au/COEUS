95a216a40287f6ddf522448de19a6c4540f85a19
Owen Holloway <owen.holloway101@gmail.com>
2019-07-28 22:31:18 +1000

    Trying to get a token endpoint working may end me

--- src/carbon.app/ClientApp/src/app/auth.config.ts
+++ src/carbon.app/ClientApp/src/app/auth.config.ts
@@ -9,6 +9,7 @@ export const authConfig: AuthConfig = {
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/index.html',
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
 
   // The SPA's id. The SPA is registered with this id at the auth-server
   clientId: 'carbon.app',
