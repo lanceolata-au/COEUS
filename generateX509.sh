71e00666b1f9aaa449d3bab288e910fe7a5fe0d0
Owen Holloway <owen.holloway101@gmail.com>
2019-09-08 16:32:33 +1000

    We need a valid X509 to make this work

---
+++ generateX509.sh
@@ -0,0 +1,3 @@
+openssl genrsa -out privatekey.pem 2048
+openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 1825
+openssl pkcs12 -export -out public_privatekey.pfx -inkey privatekey.pem -in publickey.cer
