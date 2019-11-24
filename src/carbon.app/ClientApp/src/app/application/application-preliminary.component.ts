9203a5ddd6e33d4894748ea6090567ec318b1915
Owen Holloway <owen.holloway101@gmail.com>
2019-11-25 00:06:46 +1100

    Application preliminary File cleanup in the angular app

--- src/carbon.app/ClientApp/src/app/application/application-preliminary.component.ts
+++ src/carbon.app/ClientApp/src/app/application/application-preliminary.component.ts
@@ -1,15 +1,30 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as M from 'materialize-css';
 import {getBaseUrl} from "../../main";
 import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-application-preliminary-component',
-  templateUrl: './applicationPreliminary.component.html'
+  templateUrl: './application-preliminary.component.html'
 })
 export class ApplicationPreliminaryComponent implements OnInit {
 
   ngOnInit(): void {
-     this.getApplicationStatus();
+     this.getBlankPreliminaryApplication();
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+      const elems = document.querySelectorAll('.datepicker');
+
+      const options = {
+        yearRange: 40,
+        defaultDate: new Date(1980, 1),
+        format: "dd mmm yyyy"
+      };
+
+      const instances = M.Datepicker.init(elems, options);
+
+    });
     }
   constructor(private http: HttpClient) {
   }
@@ -25,22 +40,13 @@ export class ApplicationPreliminaryComponent implements OnInit {
 
   private status;
 
-  private getApplicationStatus() {
-    this.http.get(getBaseUrl() + "Application/GetStatus").subscribe(
+  private getBlankPreliminaryApplication() {
+    this.http.get(getBaseUrl() + "Application/GetBlankPreliminaryApplication").subscribe(
       data => {
         this.status = data
       },
       error => console.log(error)
     );
-  }
-
-  private getProfile() {
-    let profileJson = sessionStorage.getItem("profile");
-
-    if (profileJson != null) {
-
-      this.profile = JSON.parse(profileJson);
-    }
   }
 
 }
